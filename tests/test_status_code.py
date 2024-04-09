ca8c6a8c7b826e0f157ea441fb8fae2e2af134d9
AlexP <lexanorilsk@gmail.com>
2024-04-09 23:16:24 +0300

    Initial commit

---
+++ tests/test_status_code.py
@@ -0,0 +1,8 @@
+import requests
+import pytest
+
+
+@pytest.mark.parametrize('url', ['https://google.com', 'https://youtube.com'])
+def test_status_code(url):
+    request = requests.get(url=url)
+    assert request.status_code == 200
