ce37fe4e7c4ec2cd2019ff5b0a0270fc3bcab5c2
AlexP <lexanorilsk@gmail.com>
2024-04-09 23:18:45 +0300

    Add comments

--- tests/test_status_code.py
+++ tests/test_status_code.py
@@ -4,5 +4,6 @@ import pytest
 
 @pytest.mark.parametrize('url', ['https://google.com', 'https://youtube.com'])
 def test_status_code(url):
+    """Simple test"""
     request = requests.get(url=url)
     assert request.status_code == 200
